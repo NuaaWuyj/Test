4ed58d1972de109a2d453487e3b7cb4f33b94f27
wuyj <zgjsnjwyj@163.com>
2022-08-19 11:40:36 +0800

    first commit

---
+++ src/main/java/com/wyj/Test.java
@@ -0,0 +1,12 @@
+package com.wyj;
+
+/**
+ * @Author Wuyj
+ * @DateTime 2022-08-19 11:39
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("软件测试...");
+    }
+}
